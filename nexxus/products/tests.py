292f7d5ee1a75c816f1aa6e172dcf9c1181ac399
Mike 0422 <mdaguillon9@gmail.com>
2024-08-11 00:49:16 -0500

    Adding Test

--- nexxus/products/tests.py
+++ nexxus/products/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase # type: ignore
+from django.urls import reverse # My Url
 
-# Create your tests here.
+from .models import Product # Model
+
+class ProductListViewTest(TestCase):
+
+    def test_should_return_200(self):
+        url = reverse('list_product') # Go back
+        response = self.client.get(url) # My Url
+        # 200 => Sucess ?
+        self.assertEqual(response.status_code, 200) # Assert => Valid result
+        self.assertEqual(response.context['products'].count(), 0) # No new products
+
+    def test_should_return_200_with_products(self):
+        url = reverse('list_product') # Go back
+        Product.objects.create( # Insert a Product
+            name='test',
+            description='test description',
+            priceCost='5',
+            priceSell='8',
+            available=True,
+            units='4',
+        )
+        response = self.client.get(url) # My Url
+        # 200 => Sucess ?
+        self.assertEqual(response.status_code, 200) # Assert => Valid result
+        self.assertEqual(response.context['products'].count(), 1) # A new product
