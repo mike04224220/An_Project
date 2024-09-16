3cc2b5e3306320a44f43cdd1b80114b3b5994c8c
Mike 0422 <mdaguillon9@gmail.com>
2024-09-15 22:49:41 -0500

    Set up

--- orders/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm  # Model form
-from .models import OrderProduct  # Model same folder
-
-
-# Model Form => Without copy field form model, only bring model's fields
-class OrderProductForm(ModelForm):
-    class Meta:  # Requirement
-        model = OrderProduct
-        fields = ["product"]  # List => Product
